cb77b40a2dbe524c60d9c96c185ad1e7f7a269c8
Beck-Haywood <beck.haywood@students.makeschool.com>
2019-10-01 12:38:09 -0700

    Can see dogs for scale In the array i made

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
 from flask import Flask, render_template, request, redirect, url_for
-from pymongo import MongoClient
+#from pymongo import MongoClient
 
 app = Flask(__name__)
 
-@app.route("/")
-def homepage():
+@app.route('/')
+def index():
+    """Return homepage."""
+    return render_template('home.html', msg='Navigate to /sell_dogs to see dogs for sale!')
 
-    pass
+# OUR MOCK ARRAY OF PROJECTS
+doginfo = [
+    { 'breed': 'Goldendoodle', 'age': '1 month' },
+    { 'breed': 'Labradoodle', 'age': '1 year' }
+]
+
+@app.route('/sell_dogs')
+def sell_dogs():
+    """Show all playlists."""
+    return render_template('sell_dogs.html', doginfo=doginfo)
 
 if __name__ == '__main__':
     app.run(debug=True)
