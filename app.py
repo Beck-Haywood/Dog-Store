679b166e3795065c58adcc0e9abb302a22dd85ab
Beck-Haywood <beck.haywood@students.makeschool.com>
2019-10-01 14:15:02 -0700

    Homepage can navigate to buy and sell dogs, however sell dogs does not add information to buy dogs yet

--- app.py
+++ app.py
@@ -1,23 +1,48 @@
 from flask import Flask, render_template, request, redirect, url_for
-#from pymongo import MongoClient
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+
+
+client = MongoClient()
+db = client.PoodleWebsite
+doginfo = db.doginfo
 
 app = Flask(__name__)
 
+#@app.route('/')
+#def index():
+#    """Return homepage."""
+#    return render_template('home.html', msg='Homepage')
+
 @app.route('/')
-def index():
-    """Return homepage."""
-    return render_template('home.html', msg='Navigate to /sell_dogs to see dogs for sale!')
-
-# OUR MOCK ARRAY OF PROJECTS
-doginfo = [
-    { 'breed': 'Goldendoodle', 'age': '1 month' },
-    { 'breed': 'Labradoodle', 'age': '1 year' }
-]
-
-@app.route('/sell_dogs')
 def sell_dogs():
-    """Show all playlists."""
-    return render_template('sell_dogs.html', doginfo=doginfo)
+    """Show all dogs for sale."""
+    return render_template('sell_dogs.html', doginfo=doginfo.find())
+@app.route('/buy')
+def buy_dogs():
+    """Show all dogs for sale."""
+    return render_template('buy_dogs.html', doginfo=doginfo.find())
+@app.route('/sell')
+def sell_new():
+    """Sell a new dog."""
+    return render_template('sell_new.html')
+
+@app.route('/', methods=['POST'])
+def dog_submit():
+    """Sell a new dog."""
+    print(request.form.to_dict())
+    return redirect(url_for('sell_new.html'))
+
+@app.route('/sell', methods=['POST'])
+def playlists_submit():
+    """Submit a new dog."""
+    doginfo = {
+        'breed': request.form.get('breed'),
+        'description': request.form.get('description')
+    }
+    doginfo_id = doginfo.insert_one(doginfo).inserted_id
+    return redirect(url_for('buy_dogs'))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
