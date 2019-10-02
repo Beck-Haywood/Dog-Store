ad8ba0fa65d8e560943abdec18b11811e237dc31
Beck-Haywood <beck.haywood@students.makeschool.com>
2019-10-01 17:15:55 -0700

    added all the app.py logic for an account and login routes

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, jsonify, json
 from pymongo import MongoClient
+from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
-
+from flask_jwt_extended import JWTManager
+from flask_jwt_extended import (create_access_token)
+from flask_bcrypt import Bcrypt
+from flask_cors import CORS
 
 client = MongoClient()
 db = client.PoodleWebsite
@@ -9,12 +13,57 @@ doginfo = db.doginfo
 
 app = Flask(__name__)
 
+app.config['MONGO_DBNAME'] = 'mernloginreg'
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/mernloginreg'
+app.config['JWT_SECRET_KEY'] = 'secret'
+
+mongo = PyMongo(app)
+bcrypt = Bcrypt(app)
+jwt = JWTManager(app)
+
+CORS(app)
+
 #@app.route('/')
 #def index():
 #    """Return homepage."""
 #    return render_template('home.html', msg='Homepage')
+@app.route('/users/register', methods=['POST'])
+def register():
+    users = mongo.db.users
+    email = request.get_json()['email']
+    password = bcrypt.generate_password_hash(request.get_json()['password'].decode('utf-8'))
 
-@app.route('/')
+    user_id = users.insert({
+    'email': email,
+    'password': password,
+    })
+
+    new_user = users.find_one({'_id' : user_id})
+    result = {'email' : new+user['email'] + ' registered'}
+    return jsonift({'result' : result})
+
+@app.route('/users/login', methods=['POST'])
+def login():
+    users = mongo.db.users
+    email = request.get_json()['email']
+    password = requests.get_json()['password']
+    result = ""
+
+    response = user.find_one({'email' : email})
+
+    if response:
+        if bcrypt.check_password_hash(response['password'], password):
+            access_token = create_access_token(identity = {
+            'email': response['email']}
+            )
+            result = jsonify({"token":access_token})
+        else:
+            result = jsonify({"error":"Invalid username and or password"})
+    else:
+        result = jsonify({"result":"no results found"})
+    return result
+
+'''@app.route('/')
 def sell_dogs():
     """Show all dogs for sale."""
     return render_template('sell_dogs.html', doginfo=doginfo.find())
@@ -74,6 +123,6 @@ def dog_delete(dog_id):
     """Delete one dog."""
     doginfo.delete_one({'_id': ObjectId(dog_id)})
     return redirect(url_for('buy_dogs'))
-
+'''
 if __name__ == '__main__':
     app.run(debug=True)
